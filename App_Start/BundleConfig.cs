710f27d6451e601bb4cc4078795cd1bf017f2369
thomasrileydev <52782404+thomasrileydev@users.noreply.github.com>
2022-04-05 12:39:27 +0100

    fixed css on create member. disabled a few lines to make it work.  05/04/2022

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,15 +8,15 @@ namespace TeamPerfomanceTracker
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        //"~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                     //   "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+          /*  bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
@@ -27,7 +27,7 @@ namespace TeamPerfomanceTracker
                       "~/Content/site.css"));
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/mobile.css",
-                      "~/Content/desktop.css"));
+                      "~/Content/desktop.css"));*/
         }
     }
 }
